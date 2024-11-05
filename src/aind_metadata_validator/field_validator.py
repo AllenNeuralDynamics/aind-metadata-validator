c56c7043ea9685ff7aae17c52daf85f538b5f758
Dan Birman <danbirman@gmail.com>
2024-11-05 13:59:42 -0800

    fix: refacotring and fixing various tests

--- src/aind_metadata_validator/field_validator.py
+++ src/aind_metadata_validator/field_validator.py
@@ -1,5 +1,6 @@
-from typing import Annotated, Optional, Union, get_args
-
+from enum import Enum
+from typing import Annotated, Optional, Union, get_args, get_origin
+import logging
 from aind_metadata_validator.utils import MetadataState
 from aind_metadata_validator.mappings import SECOND_LAYER_MAPPING
 
@@ -38,13 +39,10 @@ def validate_field_metadata(core_file_name: str, data: dict) -> dict[str, Metada
             continue
 
         if field_name not in expected_classes:
-            print(f"Warning: field name: {field_name} is missing from the expected_classes file")
+            logging.warning(f"Field name: {field_name} is missing from the expected_classes file")
             continue
 
-        # Data is present, try to coerce it to the expected class
         expected_class = expected_classes[field_name]
-
-        # Handle list/optional/union/annotated
         origin_type = getattr(expected_class, "__origin__", None)
 
         out[field_name] = validate_field(field_data, origin_type, expected_class)
@@ -53,75 +51,98 @@ def validate_field_metadata(core_file_name: str, data: dict) -> dict[str, Metada
 
 
 def validate_field(field_data, origin_type, expected_class) -> MetadataState:
-    """Validate a metadata field against its expected class
+    """Validate a metadata field against its expected class.
 
     Parameters
     ----------
     field_data : Any
-    expected_class : Class/type
+        The data to validate.
+    origin_type : Type
+        The type of the field being validated.
+    expected_class : Type
+        The expected class/type for validation.
 
     Returns
     -------
     MetadataState
-        Returns VALID or PRESENT
+        Returns VALID, PRESENT, or MISSING based on the validation.
 
     Raises
     ------
     ValueError
-        _description_
+        If validation fails.
     """
+    # If origin_type is None, we only have to validate against the expected class
+    if origin_type is None:
+        return try_instantiate(field_data, expected_class)
 
-    print((field_data, origin_type, expected_class))
-
-    # Handle optional fields that are empty
-    if origin_type is Optional:
-        validate_field_optional(field_data, expected_class)
-
-    # Annotated fields get unwrapped
+    # Handle origin_type
     if origin_type is Annotated:
         expected_class = get_args(expected_class)[0]
+        origin_type = get_origin(expected_class)
 
-    # If we're here the field is not optional
-    if not field_data or field_data == "" or field_data == {} or field_data == []:
-        return MetadataState.MISSING
-
-    # Deal with lists
     if origin_type is list:
+        return validate_field_list(field_data, expected_class)
 
-        # Handle list[Annotated[Union[...]]]
-        if getattr(expected_class.__args__[0], "__origin__", None) is Union:
-            union_types = get_args(expected_class.__args__[0])
-            return validate_field_union(field_data, union_types)
+    if origin_type is Optional:
+        return validate_field_optional(field_data, expected_class)
 
-    # Deal with unions
-    elif origin_type is Union:
+    if origin_type is Union:
         union_types = get_args(expected_class)
         return validate_field_union(field_data, union_types)
 
-    # If we're here, just validate directly
-    return try_instantiate(field_data, expected_class)
-
-
-def validate_field_optional(field_data, expected_class):
-    if field_data:
-        return validate_field(field_data, expected_class, expected_class)
-    return MetadataState.VALID
-
-
-def validate_field_union(field_data, expected_classes):
-    states = [try_instantiate(field_data, expected_class) for expected_class in expected_classes]
-
-    if MetadataState.VALID in states:
-        return MetadataState.VALID
     return MetadataState.PRESENT
 
 
+def validate_field_list(field_data, expected_class):
+    """Validate a list of data against it's expected class"""
+    if not isinstance(field_data, list):
+        return MetadataState.PRESENT
+
+    item_type = get_args(expected_class)[0]
+    origin_type = get_origin(item_type)
+
+    statuses = [validate_field(item, origin_type, item_type) for item in field_data]
+    if all(status == MetadataState.VALID for status in statuses):
+        return MetadataState.VALID
+    else:
+        return MetadataState.PRESENT
+
+
+def validate_field_optional(field_data, expected_class):
+    """Validate Optional[type] fields
+    """
+    if not field_data:
+        return MetadataState.VALID
+    return try_instantiate(field_data, expected_class)
+
+
+def validate_field_union(field_data, expected_classes):
+    """Validate Union[type, type] fields"""
+    states = [try_instantiate(field_data, cls) for cls in expected_classes]
+    if MetadataState.VALID in states:
+        return MetadataState.VALID
+    if MetadataState.PRESENT in states:
+        return MetadataState.PRESENT
+    return MetadataState.MISSING
+
+
 def try_instantiate(field_data, expected_class):
+    """Get the metadata state based on instantiating as a specific class"""
+    if expected_class is type(None):
+        return MetadataState.PRESENT if field_data else MetadataState.VALID
+    if not expected_class or not field_data:
+        return MetadataState.MISSING
+
     try:
         if isinstance(field_data, dict):
             expected_class(**field_data)
-        else:
+        elif isinstance(field_data, expected_class):
+            pass
+        elif issubclass(expected_class, Enum):
             expected_class(field_data)
+        else:
+            return MetadataState.PRESENT
         return MetadataState.VALID
     except Exception:
         return MetadataState.PRESENT
